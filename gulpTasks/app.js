f8d84c060e2a95e47912835372b7a451f76ea2c0
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-20 13:49:34 -0200

    configurando gulp

---
+++ gulpTasks/app.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp')
+
+gulp.task('app', ['app.html', 'app.css', 'app.js', 'app.assets'])
+
+
+// sempre que app.html for chamado...
+gulp.task('app.html', function() {
+
+})
+
+gulp.task('app.css', function() {
+    
+})
+
+gulp.task('app.js', function() {
+    
+})
+
+// assets = imagens, etc
+gulp.task('app.assets', function() {
+    
+})
