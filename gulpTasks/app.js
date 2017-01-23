a38c9a63e2785cbf56f5b49b0c8da8b90fd2a556
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-23 21:52:50 -0200

    dependencias html

--- gulpTasks/app.js
+++ gulpTasks/app.js
@@ -1,15 +1,18 @@
 const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
 
 gulp.task('app', ['app.html', 'app.css', 'app.js', 'app.assets'])
 
 
 // sempre que app.html for chamado...
 gulp.task('app.html', function() {
-
+    gulp.src('app/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('public'))
 })
 
 gulp.task('app.css', function() {
-    
+
 })
 
 gulp.task('app.js', function() {
