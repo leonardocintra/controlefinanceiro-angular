57282cee98cd2fbbd50cb2adc1c72bf5c085a1db
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-23 21:59:50 -0200

    finalizado build dos arquivos do projeto

--- gulpTasks/app.js
+++ gulpTasks/app.js
@@ -1,5 +1,10 @@
 const gulp = require('gulp')
 const htmlmin = require('gulp-htmlmin')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const babel = require('gulp-babel')
+
 
 gulp.task('app', ['app.html', 'app.css', 'app.js', 'app.assets'])
 
@@ -12,14 +17,23 @@ gulp.task('app.html', function() {
 })
 
 gulp.task('app.css', function() {
-
+    gulp.src('app/**/*.css')
+        .pipe(uglifycss({ "uglyComments": true} ))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('public/assets/css'))        
 })
 
 gulp.task('app.js', function() {
-    
+    gulp.src('app/**/*.js')
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('public/assets/js'))
 })
 
 // assets = imagens, etc
 gulp.task('app.assets', function() {
+    gulp.src('assets/**/*.*')
+        .pipe(gulp.dest('public/assets'))
     
 })
