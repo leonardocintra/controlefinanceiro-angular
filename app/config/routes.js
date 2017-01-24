218bdfbba3d5017c2fc2abe6772a427c51c0b7e8
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-24 12:36:12 -0200

    implementando link

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,8 +1,19 @@
 // esses modulos fazem parte do ui-router (angular)
+// templates sao carregados no ui-view no index.hmml
+// ui-sref = state('dashboard'...)
+
 angular.module('primeiraApp').config([
     '$stateProvider',
     '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
-        
+        $stateProvider.state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "dashboard/dashboard.html"
+        }).state('billingCycle', {
+            url: "/billingCycles",
+            templateUrl: "billingCycle/tabs.html"
+        })
+
+        $urlRouterProvider.otherwise('/dashboard')
     }
 ])
