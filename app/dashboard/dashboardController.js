173af59b6caf627700500a4518d59657f46ed385
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-30 18:17:40 -0200

    refatoring to angular 1.6

---
+++ app/dashboard/dashboardController.js
@@ -0,0 +1,22 @@
+(function() {
+    angular.module('primeiraApp').controller('DashboardCtrl', [
+        '$http',
+        DasboardController
+    ])
+
+    function DasboardController($http) {
+        const vm = this
+        vm.getSummary = function() {
+            const url = 'https://controlefinanceiro-api.herokuapp.com/api/billingSummary'
+            $http.get(url).then(function(response) {
+                const { credit = 0, debt = 0} = response.data
+                vm.credit = credit
+                vm.debt = debt
+                vm.total = credit - debt    
+            })
+        }
+
+        vm.getSummary()
+    }
+})()
+
