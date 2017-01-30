e1cdcb351752b2d481bddc590b13afcffbdae227
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-30 15:36:37 -0200

    com erro no angular

--- app/common/components/valueBox.js
+++ app/common/components/valueBox.js
@@ -9,21 +9,23 @@ angular.module('primeiraApp').component('valueBox', {
     controller: [
         'gridSystem',
         function(gridSystem) {
-            this.gridClasses = gridSystem.toCssClasses(this.grid)
-
+            const self = this
+            this.$onInit = function() {
+                self.gridClasses = gridSystem.toCssClasses(self.grid)
+            };
         }
     ],
     template: `
-        <div class="{{ $ctrl.gridClasses }}">
-            <div class="small-box {{ $ctrl.colorClass }}">
-                <div class="inner">
-                    <h3>{{ $ctrl.value }}</h3>
-                    <p>{{ $ctrl.text }}</p>
-                </div>
-                <div class="icon">
-                    <i class="fa {{ $ctrl.iconClass }}"></i>
-                </div>
+    <div class="{{ $ctrl.gridClasses }}">
+        <div class="small-box {{ $ctrl.colorClass }}">
+            <div class="inner">
+                <h3>{{ $ctrl.value }}</h3>
+                <p>{{ $ctrl.text }}</p>
+            </div>
+            <div class="icon">
+                <i class="fa {{ $ctrl.iconClass }}"></i>
             </div>
         </div>
+    </div>
     `
 })
