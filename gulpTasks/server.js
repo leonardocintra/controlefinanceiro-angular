d3b502702d344cc6894e5dc281a254a6a69b0025
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-30 16:25:43 -0200

    retentando publish to heroku

--- gulpTasks/server.js
+++ gulpTasks/server.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp')
 const watch = require('gulp-watch')
 const webserver = require('gulp-webserver')
+const port = process.env.PORT || 3000
 
 // para entender esses esquema, ver aula 62
 // https://www.udemy.com/mean-primeira-aplicacao-do-zero/learn/v4/t/lecture/6192062
 gulp.task('server', ['watch'], function() {
     gulp.src('public').pipe(webserver({
         livereload: true,
-        port: 3000,
+        port: port,
         open: true
     }))
 })
