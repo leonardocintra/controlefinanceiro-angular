59e029f3649ceb74353700893ac282f460a63713
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-31 12:51:58 -0200

    usando tabs activas

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -34,6 +34,17 @@
             })
         }
 
+        
+        vm.showTabUpdate = function(billingCycle) {
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {tabUpdate: true})
+        }
+
+        vm.showTabDelete = function(billingCycle) {
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {tabDelete: true})
+        }
+
         vm.refresh()
     }
 
